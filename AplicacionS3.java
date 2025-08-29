fa2e645a5f33334262e5edeaea2565e7aca2a20b
Felipe <feliijna@gmail.com>
2025-08-28 21:45:52 -0400

    se agrego escaner para catura las entradas

--- AplicacionS3.java
+++ AplicacionS3.java
@@ -11,12 +11,24 @@ import java.util.Scanner;
  * @author HP
  */
 public class AplicacionS3 {
+scaner sc = new scanner(System.in);
 string Marca ;
 string Modelo  ;
 int Cilindrada ;
 string Tipo_de_Combustible  ;
 int numero_de_personas  ;
-system.;out.println("Ingrese la marca del vehiculo: ");
+system.out.println("escriva la marca");
+stirng Marca = sc.nextline();
+system.out.println("escriva el modelo");
+string Modelo = sc.nextline();
+system.out.println("escriva la cilindrada");
+int Cilindrada = sc.nextint();
+system.out.println("escriva el tipo de combustible");
+string Tipo_de_Combustible = sc.nextline();
+system.out.println("escriva el numero de personas");
+int numero_de_personas = sc.nextint();
+
+    
 
 
 }
