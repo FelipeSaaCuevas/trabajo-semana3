0a1e082f393c036f60ec511bfbdc867c4fe770f0
Felipe <feliijna@gmail.com>
2025-08-28 21:34:35 -0400

    se agrega el archivoy se crean las variables

---
+++ AplicacionS3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplicacion.s3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class AplicacionS3 {
+string Marca ;
+string Modelo  ;
+int Cilindrada ;
+string Tipo_de_Combustible  ;
+int numero_de_personas  ;
+system.;out.println("Ingrese la marca del vehiculo: ");
+
+
+}
