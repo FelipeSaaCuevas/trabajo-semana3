626947a5bc627948357f407ae8540c37f87c42c9
Felipe <feliijna@gmail.com>
2025-08-29 19:42:13 -0400

    se agrego las salilidas de informacion

--- AplicacionS3.java
+++ AplicacionS3.java
@@ -17,18 +17,56 @@ string Modelo  ;
 int Cilindrada ;
 string Tipo_de_Combustible  ;
 int numero_de_personas  ;
-system.out.println("escriva la marca");
-stirng Marca = sc.nextline();
+int km;
+System.out.println("escriva la marca");
+Stirng Marca = sc.nextline();
 system.out.println("escriva el modelo");
-string Modelo = sc.nextline();
-system.out.println("escriva la cilindrada");
+String Modelo = sc.nextline();
+System.out.println("escriva la cilindrada");
 int Cilindrada = sc.nextint();
-system.out.println("escriva el tipo de combustible");
-string Tipo_de_Combustible = sc.nextline();
-system.out.println("escriva el numero de personas");
+System.out.println("escriva el tipo de combustible");
+String Tipo_de_Combustible = sc.nextline();
+System.out.println("escriva el numero de personas");
 int numero_de_personas = sc.nextint();
+system.out.println("cuentos kilometros")
+int km = sc.nextint();
+system.out.println("cuento paga")
+int Dinero = sc.nextint();
+int opciones;
+if (Dinero > 49999) {
+            opciones = 1;
+        } else {
+            opciones = 2;
+ }
 
-    
+        switch (opciones) {
+            case 1:
+                if (km >= 20) {
+                    System.out.println("Se cobra un monto extra por sobrepasar los 20 km");
+                    int ex = 150;
+                    int total = ex * Dinero;
+                    System.out.println("Su cuenta es de " + total +", por " + Marca + " " + Modelo +", cilindrada de motor: " + Cilindrada +", tipo de combustible: " + Tipo_de_Combustible +", número de personas: " + numero_de_personas);
+                } else {
+                    int ex = 1;
+                    int total = ex * Dinero;
+                    System.out.println("Su cuenta es de " + total +", por " + Marca + " " + Modelo +", cilindrada de motor: " + Cilindrada +", tipo de combustible: " + Tipo_de_Combustible +", número de personas: " + numero_de_personas);
+                }
+                break;
 
+            case 2:
+                if (km >= 20) {
+                    System.out.println("Se cobra un monto extra por sobrepasar los 20 km");
+                    int ex = 150;
+                    int total = ex * Dinero;
+                    System.out.println("Su cuenta es de " + total +", por " + Marca + " " + Modelo +", cilindrada de motor: " + Cilindrada +", tipo de combustible: " + Tipo_de_Combustible +", número de personas: " + numero_de_personas);
+                } else {
+                    int ex = 1;
+                    int total = ex * Dinero;
+                    System.out.println("Su cuenta es de " + total +", por " + Marca + " " + Modelo +", cilindrada de motor: " + Cilindrada +", tipo de combustible: " + Tipo_de_Combustible +", número de personas: " + numero_de_personas);
+                }
+                break;
+        }
 
+        sc.close();
+    }
 }
