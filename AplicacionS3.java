0910c74e3b84330fe2f20edf42a0469a5a5ea12e
Felipe <feliijna@gmail.com>
2025-08-30 09:33:14 -0400

    codigo corregido

--- AplicacionS3.java
+++ AplicacionS3.java
@@ -6,38 +6,45 @@ package com.mycompany.aplicacion.s3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author HP
- */
 public class AplicacionS3 {
-scaner sc = new scanner(System.in);
-string Marca ;
-string Modelo  ;
-int Cilindrada ;
-string Tipo_de_Combustible  ;
-int numero_de_personas  ;
-int km;
-System.out.println("escriva la marca");
-Stirng Marca = sc.nextline();
-system.out.println("escriva el modelo");
-String Modelo = sc.nextline();
-System.out.println("escriva la cilindrada");
-int Cilindrada = sc.nextint();
-System.out.println("escriva el tipo de combustible");
-String Tipo_de_Combustible = sc.nextline();
-System.out.println("escriva el numero de personas");
-int numero_de_personas = sc.nextint();
-system.out.println("cuentos kilometros")
-int km = sc.nextint();
-system.out.println("cuento paga")
-int Dinero = sc.nextint();
-int opciones;
-if (Dinero > 49999) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String Marca;
+        String Modelo;
+        int Cilindrada;
+        String Tipo_de_Combustible;
+        int numero_de_personas;
+        int km;
+
+        System.out.println("Escriba la marca");
+        Marca = sc.nextLine();
+
+        System.out.println("Escriba el modelo");
+        Modelo = sc.nextLine();
+
+        System.out.println("Escriba la cilindrada");
+        Cilindrada = sc.nextInt();
+        sc.nextLine(); // limpiar buffer
+
+        System.out.println("Escriba el tipo de combustible");
+        Tipo_de_Combustible = sc.nextLine();
+
+        System.out.println("Escriba el número de personas");
+        numero_de_personas = sc.nextInt();
+
+        System.out.println("¿Cuántos kilómetros?");
+        km = sc.nextInt();
+
+        System.out.println("¿Cuánto paga?");
+        int Dinero = sc.nextInt();
+
+        int opciones;
+        if (Dinero > 49999) {
             opciones = 1;
         } else {
             opciones = 2;
- }
+        }
 
         switch (opciones) {
             case 1:
@@ -45,11 +52,17 @@ if (Dinero > 49999) {
                     System.out.println("Se cobra un monto extra por sobrepasar los 20 km");
                     int ex = 150;
                     int total = ex * Dinero;
-                    System.out.println("Su cuenta es de " + total +", por " + Marca + " " + Modelo +", cilindrada de motor: " + Cilindrada +", tipo de combustible: " + Tipo_de_Combustible +", número de personas: " + numero_de_personas);
+                    System.out.println("Su cuenta es de " + total + ", por " + Marca + " " + Modelo +
+                            ", cilindrada de motor: " + Cilindrada +
+                            ", tipo de combustible: " + Tipo_de_Combustible +
+                            ", número de personas: " + numero_de_personas);
                 } else {
                     int ex = 1;
                     int total = ex * Dinero;
-                    System.out.println("Su cuenta es de " + total +", por " + Marca + " " + Modelo +", cilindrada de motor: " + Cilindrada +", tipo de combustible: " + Tipo_de_Combustible +", número de personas: " + numero_de_personas);
+                    System.out.println("Su cuenta es de " + total + ", por " + Marca + " " + Modelo +
+                            ", cilindrada de motor: " + Cilindrada +
+                            ", tipo de combustible: " + Tipo_de_Combustible +
+                            ", número de personas: " + numero_de_personas);
                 }
                 break;
 
@@ -58,11 +71,17 @@ if (Dinero > 49999) {
                     System.out.println("Se cobra un monto extra por sobrepasar los 20 km");
                     int ex = 150;
                     int total = ex * Dinero;
-                    System.out.println("Su cuenta es de " + total +", por " + Marca + " " + Modelo +", cilindrada de motor: " + Cilindrada +", tipo de combustible: " + Tipo_de_Combustible +", número de personas: " + numero_de_personas);
+                    System.out.println("Su cuenta es de " + total + ", por " + Marca + " " + Modelo +
+                            ", cilindrada de motor: " + Cilindrada +
+                            ", tipo de combustible: " + Tipo_de_Combustible +
+                            ", número de personas: " + numero_de_personas);
                 } else {
                     int ex = 1;
                     int total = ex * Dinero;
-                    System.out.println("Su cuenta es de " + total +", por " + Marca + " " + Modelo +", cilindrada de motor: " + Cilindrada +", tipo de combustible: " + Tipo_de_Combustible +", número de personas: " + numero_de_personas);
+                    System.out.println("Su cuenta es de " + total + ", por " + Marca + " " + Modelo +
+                            ", cilindrada de motor: " + Cilindrada +
+                            ", tipo de combustible: " + Tipo_de_Combustible +
+                            ", número de personas: " + numero_de_personas);
                 }
                 break;
         }
